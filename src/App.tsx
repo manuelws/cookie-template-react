5e0062fe29696079ef353dbe3956f318434c2e61
manuelws <75837144+manuelws@users.noreply.github.com>
2023-07-26 10:04:29 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Cookie from './components/Cookie'
+import Debug from './components/Debug'
+
+function App() {
+  return (
+    <div className="page">
+      <h1 className="page-title">Cookie Banner with React and TypeScript</h1>
+      <Cookie />
+      <Debug />
+    </div>
+  )
+}
+
+export default App
